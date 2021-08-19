a19c77e7e6b853d9df1578e866ee835d9f6d04fa
Hilman Syafei <hilman@alterra.id>
2021-08-19 11:17:21 +0700

    add check status delete old

--- process_trx.php
+++ process_trx.php
@@ -1,18 +1,20 @@
 <?php
 require_once 'PHPExcel.php';
 require_once 'vendor/autoload.php';
-
 $client = new GuzzleHttp\Client();
 
-
 echo '<pre>';
+// get all data post
 $getDataPost = $_POST;
 
-if (empty($_POST)) {
+// check emty data
+if (empty($getDataPost)) {
   echo "Silahkan akses melalui form \n";
-  echo "<a href='http://localhost/bulk_upload' >Kembali </a>";
+  echo "<a href='http://localhost/bulk_upload'> Kembali </a>";
   die();
 }
+
+// initiate data
 $username = $getDataPost['username'];
 $password = $getDataPost['password'];
 $pin = $getDataPost['pin'];
@@ -23,51 +25,76 @@ $uploaddir = 'file_bulk/';
 // check file for safety
 $checkFile = scandir($uploaddir);
 if (count($checkFile) > 2) {
-  echo "Silahkan hapus file pada folder $uploaddir \n";
-  echo "<a href='http://localhost/bulk_upload' >Kembali </a>";
-  die();
+    // check delete old mode
+    if (!isset($_POST['delete_old'])) {
+        echo "Silahkan hapus file pada folder $uploaddir \n";
+        echo "<a href='http://localhost/bulk_upload'> Kembali </a>";
+        die();  
+    }
+    // delete file
+    unlink($uploaddir.$checkFile[2]);
 }
 
+// initiate file name
 $fileName = $_FILES['file_bulk']['name'];
 $splitName = explode(".", $fileName);
 $newFileName = time().".".end($splitName);
 $uploadfile = $uploaddir . basename($newFileName);
 
-
+// upload file
 if (move_uploaded_file($_FILES['file_bulk']['tmp_name'], $uploadfile)) {
-  echo "File is valid, and was successfully uploaded.\n\n";
+    echo "File is valid, and was successfully uploaded.\n\n";
 } else {
-  echo "Failed upload file!\n";
-  die();
+    echo "Failed upload file!\n";
+    die();
 }
 
+// initiate excel reader
 $objPHPExcel = PHPExcel_IOFactory::load($uploadfile);
 $worksheet = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-
 $report = array();
 
+// loop data excel
 for ($i=2; $i <= count($worksheet); $i++) { 
-  $noTujuan = $worksheet[$i]['A'];
-  $kdProduk = $worksheet[$i]['B'];
-  $trxOrderID = bin2hex(random_bytes(13));
+    $noTujuan = str_replace("'","",$worksheet[$i]['A']);
+    if ($noTujuan != "") {
+        $kdProduk = $worksheet[$i]['B'];
+        $trxOrderID = bin2hex(random_bytes(13));
 
-  $res = $client->request('GET', "$host?id=$id&pin=$pin&user=$username&pass=$password&kodeproduk=$kdProduk&tujuan=$noTujuan", [
-    'connect_timeout' => 60,
-    'timeout' => 60
-  ]);
+        // hit trx
+        $res = $client->request('GET', "$host?id=$id&pin=$pin&user=$username&pass=$password&kodeproduk=$kdProduk&tujuan=$noTujuan&idtrx=$trxOrderID", [
+            'connect_timeout' => 60,
+            'timeout' => 60,
+            'http_errors' => false
+        ]);
 
-  echo "Order ID : $trxOrderID \n";
-  echo "No Tujuan : $noTujuan \n";
-  echo "Produk Alias : $kdProduk \n";
-  echo "Status : ".$res->getStatusCode()." \n";
-  echo "Body : ".$res->getBody()." \n\n";
+        echo "Order ID : $trxOrderID \n";
+        echo "No Tujuan : $noTujuan \n";
+        echo "Produk Alias : $kdProduk \n";
+        echo "Status Code: ".$res->getStatusCode()." \n";
+        echo "Body Response : ".$res->getBody()." \n\n";
 
-  if (array_key_exists($res->getStatusCode(),$report)) {
-    $report[$res->getStatusCode()] = $report[$res->getStatusCode()] + 1;
-  }else{
-    $report[$res->getStatusCode()] = 1;
-  }
+        // collect data by status code
+        if (array_key_exists($res->getStatusCode(),$report)) {
+            $report[$res->getStatusCode()] = $report[$res->getStatusCode()] + 1;
+        }else{
+            $report[$res->getStatusCode()] = 1;
+        }
+        sleep(0.5);
+    }
 }
 
-print_r($report);
+// generate report information
+echo "-----------------------------------------\n";
+echo "<b>REPORT \n</b>";
+echo "-----------------------------------------\n";
+foreach ($report as $key => $value) {
+    if ($key != 201) {
+        echo "Transaksi Gagal (".$key.") : ".$value."\n";
+    }else{
+        echo "Transaksi Success (".$key.") : ".$value."\n";
+    }
+}
+echo "-----------------------------------------\n";
+echo "<a href='http://localhost/bulk_upload'> Kembali </a>";
 ?>
