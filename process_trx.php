f7e55763fe3a295e7e0d869fb16fd11055e2b593
Hilman Syafei <hilman@alterra.id>
2021-08-20 15:45:40 +0700

    improve result

--- process_trx.php
+++ process_trx.php
@@ -42,9 +42,7 @@ $newFileName = time().".".end($splitName);
 $uploadfile = $uploaddir . basename($newFileName);
 
 // upload file
-if (move_uploaded_file($_FILES['file_bulk']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n\n";
-} else {
+if (!move_uploaded_file($_FILES['file_bulk']['tmp_name'], $uploadfile)) {
     echo "Failed upload file!\n";
     die();
 }
@@ -53,12 +51,14 @@ if (move_uploaded_file($_FILES['file_bulk']['tmp_name'], $uploadfile)) {
 $objPHPExcel = PHPExcel_IOFactory::load($uploadfile);
 $worksheet = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
 $report = array();
+$reportData = array();
 
 // loop data excel
 for ($i=2; $i <= count($worksheet); $i++) { 
     $noTujuan = str_replace("'","",$worksheet[$i]['A']);
     if ($noTujuan != "") {
         $kdProduk = $worksheet[$i]['B'];
+        // generate order ID
         $trxOrderID = bin2hex(random_bytes(13));
 
         // hit trx
@@ -68,11 +68,16 @@ for ($i=2; $i <= count($worksheet); $i++) {
             'http_errors' => false
         ]);
 
-        echo "Order ID : $trxOrderID \n";
-        echo "No Tujuan : $noTujuan \n";
-        echo "Produk Alias : $kdProduk \n";
-        echo "Status Code: ".$res->getStatusCode()." \n";
-        echo "Body Response : ".$res->getBody()." \n\n";
+        $dataTrx = array(
+            'order_id' => $trxOrderID,
+            'tujuan' => $noTujuan,
+            'produk_alias' => $kdProduk,
+            'status_code' => $res->getStatusCode(),
+            'body' => $res->getBody(),
+            'date' => date("d-m-Y H:i:s")
+        );
+
+        array_push($reportData, $dataTrx);
 
         // collect data by status code
         if (array_key_exists($res->getStatusCode(),$report)) {
@@ -96,5 +101,35 @@ foreach ($report as $key => $value) {
     }
 }
 echo "-----------------------------------------\n";
-echo "<a href='http://localhost/bulk_upload'> Kembali </a>";
+echo "<a href='http://localhost/bulk_upload'> Kembali </a> \n\n";
+
+echo "<table border=1>
+        <tr>
+            <td>No</td>
+            <td align='center'>Order ID</td>
+            <td align='center'>Tujuan</td>
+            <td>Produk Alias</td>
+            <td>Status Code</td>
+            <td align='center'>Body</td>
+            <td>Created Date</td>
+        </tr>";
+$no = 1;
+foreach ($reportData as $key => $value) {
+    $statusCode = $value['status_code'];
+    if ($value['status_code'] != 201) {
+        $statusCode = "<b style='color:red'>".$value['status_code']."</b>";
+    }
+    echo "<tr>
+            <td>$no</td>
+            <td>".$value['order_id']."</td>
+            <td>'".$value['tujuan']."</td>
+            <td>".$value['produk_alias']."</td>
+            <td>".$statusCode."</td>
+            <td>".$value['body']."</td>
+            <td>".$value['date']."</td>
+        </tr>";
+    $no++;
+}
+echo "</table>";
+
 ?>
